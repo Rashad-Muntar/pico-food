f098c9155b8cb27bfa808c0b8d8a853a1eb90903
Rashad-Muntar <muntartoure@gmail.com>
2021-10-13 17:57:17 +0000

    Add Side bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Badge from '@mui/material/Badge';
 import ShoppingBasketTwoToneIcon from '@mui/icons-material/ShoppingBasketTwoTone';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import styles from '../assets/styles/navbar.module.css';
 
-const Navbar = ({ showCart, cartCount }) => (
-  <nav className={styles.navbar}>
-    <div className={styles.contentWrapper}>
-      <a href="google.com" className={styles.brand}>
-        <FastfoodIcon />
-        PICO
-        <span className={styles.span}>FOOD</span>
-      </a>
+const Navbar = ({ showCart, cartCount }) => {
+  const [sideBarShow, setSideBarShow] = useState(false);
 
-      <div className={styles.navLinksArea}>
+  const handleMobileMenu = () => {
+    setSideBarShow(true);
+  };
+
+  const closeMobileMenuHandler = () => {
+    setSideBarShow(false);
+  };
+
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.contentWrapper}>
+          <a href="google.com" className={styles.brand}>
+            <FastfoodIcon />
+            PICO
+            <span className={styles.span}>FOOD</span>
+          </a>
+
+          <div className={styles.navLinksArea}>
+            <div className={styles.menuItems}>
+              <a href="google.com" className={styles.menuLink}>
+                Menu
+              </a>
+              <a href="google.com" className={styles.menuLink}>
+                Services
+              </a>
+              <a href="google.com" className={styles.menuLink}>
+                Gallery
+              </a>
+            </div>
+            <div className={styles.right}>
+              <button type="button" className={styles.cartLink} onClick={showCart}>
+                <Badge badgeContent={cartCount} color="primary">
+                  <ShoppingBasketTwoToneIcon />
+                </Badge>
+              </button>
+            </div>
+          </div>
+          <div className={styles.mobileRightArea}>
+            <div className={styles.right}>
+              <button type="button" className={styles.cartLink} onClick={showCart}>
+                <Badge badgeContent={cartCount} color="primary">
+                  <ShoppingBasketTwoToneIcon />
+                </Badge>
+              </button>
+            </div>
+            <MenuIcon onClick={handleMobileMenu} />
+          </div>
+        </div>
+      </nav>
+
+      {sideBarShow && (
+      <div className={styles.navLinksAreaForMobile}>
+        <CloseIcon className={styles.closeBtn} onClick={closeMobileMenuHandler} />
         <div className={styles.menuItems}>
           <a href="google.com" className={styles.menuLink}>
             Menu
@@ -26,17 +75,11 @@ const Navbar = ({ showCart, cartCount }) => (
             Gallery
           </a>
         </div>
-        <div className={styles.right}>
-          <button type="button" className={styles.cartLink} onClick={showCart}>
-            <Badge badgeContent={cartCount} color="primary">
-              <ShoppingBasketTwoToneIcon />
-            </Badge>
-          </button>
-        </div>
       </div>
-    </div>
-  </nav>
-);
+      )}
+    </>
+  );
+};
 
 Navbar.propTypes = {
   showCart: PropTypes.node.isRequired,
