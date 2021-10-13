239d389bccac7d7571b635013b85fda98d121250
Rashad-Muntar <muntartoure@gmail.com>
2021-10-13 23:40:44 +0000

    Add styling for tablet view

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,42 +4,42 @@ const Menu = () => (
   <section className={styles.menuCardWrapper}>
     <div className={styles.menuCard}>
       <div className={styles.icons}>
-        <img src="https://img.icons8.com/dotty/80/000000/pizza.png" alt="card-img" />
+        <img className={styles.menuIcon} src="https://img.icons8.com/dotty/80/000000/pizza.png" alt="card-img" />
       </div>
       <p className={styles.name}>Pizza</p>
     </div>
 
     <div className={styles.menuCard}>
       <div className={styles.icons}>
-        <img src="https://img.icons8.com/ios/50/000000/salad--v1.png" alt="card-img" />
+        <img className={styles.menuIcon} src="https://img.icons8.com/ios/50/000000/salad--v1.png" alt="card-img" />
       </div>
       <p className={styles.name}>Salad</p>
     </div>
 
     <div className={styles.menuCard}>
       <div className={styles.icons}>
-        <img src="https://img.icons8.com/ios/50/000000/hamburger.png" alt="card-img" />
+        <img className={styles.menuIcon} src="https://img.icons8.com/ios/50/000000/hamburger.png" alt="card-img" />
       </div>
       <p className={styles.name}>Hamburger</p>
     </div>
 
     <div className={styles.menuCard}>
       <div className={styles.icons}>
-        <img alt="card-img" src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/60/000000/external-drink-carnival-vitaliy-gorbachev-lineal-vitaly-gorbachev.png" />
+        <img className={styles.menuIcon} alt="card-img" src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/60/000000/external-drink-carnival-vitaliy-gorbachev-lineal-vitaly-gorbachev.png" />
       </div>
       <p className={styles.name}>Drinks</p>
     </div>
 
     <div className={styles.menuCard}>
       <div className={styles.icons}>
-        <img alt="card-img" src="https://img.icons8.com/external-photo3ideastudio-lineal-photo3ideastudio/64/000000/external-sandwich-august-bakery-photo3ideastudio-lineal-photo3ideastudio.png" />
+        <img className={styles.menuIcon} alt="card-img" src="https://img.icons8.com/external-photo3ideastudio-lineal-photo3ideastudio/64/000000/external-sandwich-august-bakery-photo3ideastudio-lineal-photo3ideastudio.png" />
       </div>
       <p className={styles.name}>Sandwich</p>
     </div>
 
     <div className={styles.menuCard}>
       <div className={styles.icons}>
-        <img alt="card-img" src="https://img.icons8.com/external-flatart-icons-outline-flatarticons/64/000000/external-chicken-supermarket-flatart-icons-outline-flatarticons.png" />
+        <img className={styles.menuIcon} alt="card-img" src="https://img.icons8.com/external-flatart-icons-outline-flatarticons/64/000000/external-chicken-supermarket-flatart-icons-outline-flatarticons.png" />
       </div>
       <p className={styles.name}>Fried Chicken</p>
     </div>
